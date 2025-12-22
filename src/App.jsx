0d143f6b30d1271515756c66d013e987a0df1abe
abraham <abrahamkifle1212@gmail.com>
2025-12-22 14:41:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './assets/img/logo.svg'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div >
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import logo from '../assets/img/logo.svg'
+
+function Navbar() {
+  return (
+   <div >
+         {/*  Navbar */}
+         <nav className="relative container mx-auto p-6">
+           {/* Flex container */}
+           <div className="flex items-center justify-between">
+             {/* Logo */}
+             <div className="pt-2">
+               <img src={logo} alt="" />
+             </div>
+             {/* Menu Items  */}
+             <div className=" hidden md:flex space-x-6">
+               <a href="#" className='hover:text-darkGrayishBlue'>Pricing</a>
+               <a href="#" className='hover:text-darkGrayishBlue'>Product</a>
+               <a href="#" className='hover:text-darkGrayishBlue'>About Us</a>
+               <a href="#" className='hover:text-darkGrayishBlue'>Careers</a>
+               <a href="#" className='hover:text-darkGrayishBlue'>Community</a>
+             </div>
+             {/* Button */}
+             <a href="#" className='hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight'>Get Started</a>
+           </div>
+         </nav>
+       </div>
+  )
+}
+
+export default Navbar
