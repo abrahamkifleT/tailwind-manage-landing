c968bee2bb8feb8d2654a6b00f41ca7988d6490c
abraham <abrahamkifle1212@gmail.com>
2025-12-22 15:13:47 +0300

    adding hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import logo from './assets/img/logo.svg'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
 
   return (
     <div >
       <Navbar />
+      <Hero />
     </div>
   )
 }
